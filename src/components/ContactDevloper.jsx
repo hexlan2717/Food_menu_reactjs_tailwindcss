0eac85adf4b52c60395e5b6020f40f112683f97c
hexlan2717 <hexlan2717@gmail.com>
2022-08-13 23:19:00 +0530

    Menu created

---
+++ src/components/ContactDevloper.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const ContactDevloper = () => {
+  return (
+    <div className="pt-6">
+        <h3 className="font-Bree text-white">Made by CLBS LABS</h3>
+    </div>
+  )
+}
+
+export default ContactDevloper
